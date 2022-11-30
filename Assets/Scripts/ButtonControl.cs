b376f6b3771f7a9bfefa5772d624d5e962551bbc
I-hsin-Chen <ihsin105521@gmail.com>
2022-11-30 18:39:59 +0800

    level 1 complete

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer renderer;
+    private BoxCollider2D collider;
+
+    private ContactFilter2D pressFilter;
+    private List<ContactPoint2D> contactBuffer = new List<ContactPoint2D>();
+    public bool pressed { get; private set; } = false;
+
+    public Sprite pressedImage;
+    public GameObject door;
+
+    private void Awake(){
+        TryGetComponent<Rigidbody2D>(out rb);
+        TryGetComponent<SpriteRenderer>(out renderer);
+        TryGetComponent<BoxCollider2D>(out collider);
+
+        pressFilter.useNormalAngle = true;
+        pressFilter.minNormalAngle = -45.0f - 90.0f;
+        pressFilter.maxNormalAngle = -45.0f;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pressed){
+            int groundHits = rb.GetContacts(pressFilter, contactBuffer);
+            print(groundHits);
+            if (groundHits > 0){
+                pressed = true;
+                renderer.sprite = pressedImage;
+
+                Vector2 size = collider.size;
+                collider.size = new Vector2 (collider.size.x, collider.size.y/2);
+                door.GetComponent<Animator>().enabled = true;
+            }
+        }
+    }
+}
