0614b0616165065a0401970740868a37839b399c
PaulWang0513 <paul05130125@gmail.com>
2022-12-04 05:15:08 +0800

    win() function done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,4 +20,36 @@ public class GameManager : MonoBehaviour
             UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(2);
         }
     }
+
+    // call this public function when win
+    // this function will load next scene with fade out effect
+    // don't forget to set the index of next scene
+    public void Win(int nextSceneIndex)
+    {
+        Debug.Log("Win");
+        // To make the game run smoothly, destroy some DontDestroyOnLoad objects
+        // ==========
+        GameObject toDestroy;
+        toDestroy = GameObject.Find("PlayText.PlayTextSupport.EventCenter");
+        if (toDestroy != null)
+            Destroy(toDestroy);
+        toDestroy = GameObject.Find("PlayText.PlayTextSupport.ResMgr");
+        if (toDestroy != null)
+            Destroy(toDestroy);
+        toDestroy = GameObject.Find("PlayText.PlayTextSupport.AudioMgr");
+        if (toDestroy != null)
+            Destroy(toDestroy);
+        // ==========
+        float fadeOutTime = 1.0f;
+        GameObject fadeCanvas = GameObject.Find("FadeCanvas");
+        fadeCanvas.GetComponent<FadeHandler>().StartFadeOut(fadeOutTime);
+        StartCoroutine(ScheduleChangeScene(fadeOutTime, nextSceneIndex));
+    }
+
+    IEnumerator ScheduleChangeScene(float fadeOutTime, int idx){
+        yield return new WaitForSeconds(fadeOutTime);
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(idx);
+        Time.timeScale = 1;
+    }
+    
 }
