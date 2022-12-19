37a15e2716b1361cd37e9cd0d2f0f6e349b3a936
PaulWang0513 <paul05130125@gmail.com>
2022-12-19 23:04:52 +0800

    Finished Prologue object, ready to add scene and story

---
+++ Assets/Scripts/Prologue.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+// This script plays the prologue story.
+
+public class Prologue : MonoBehaviour
+{
+    private GameObject textObject;                      // The text GameObject in the Canvas
+    private List<string> contents = new List<string>(); // The contents to be displayed sequentially
+    private float fadeInTime;                           // The time it takes for the text to fade in
+    private float duration;                             // The time the text is displayed
+    private float fadeOutTime;                          // The time it takes for the text to fade out
+    private float delay;                                // The time between the end of the text and the start of the next text
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the text object
+        textObject = GameObject.Find("Prologue/Canvas/Text");
+        if (textObject != null) {
+            Debug.Log("Text object found");
+        }
+        // Initialize the text to be empty and invisible
+        textObject.GetComponent<TextMeshProUGUI>().text = "";
+        textObject.GetComponent<TextMeshProUGUI>().alpha = 0.0f;
+        // Initialize the contents, just for testing now
+        contents.Add("ouo");
+        contents.Add("ouob");
+        contents.Add("douob");
+        contents.Add("dOuOb");
+        contents.Add("dOUOb");
+        // Initialize the fade in/out time and duration
+        fadeInTime = 1.0f;
+        duration = 2.0f;
+        fadeOutTime = 1.0f;
+        delay = 0.5f;
+
+        // Start the prologue
+        StartPrologue();
+    }
+
+    public void StartPrologue(){
+        Debug.Log("Start Prologue");
+        StartCoroutine(SchedulePrologue(0));
+    }
+
+    IEnumerator SchedulePrologue(int contents_index) {
+        // If the index is out of range, stop the prologue and change to the next scene
+        if (contents_index >= contents.Count) {
+            Debug.Log("End of prologue");
+            // Change to the next scene
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Test");
+            yield return null;
+        }
+
+        float startTime;
+        float endTime;
+        float alpha;
+        textObject.GetComponent<TextMeshProUGUI>().text = contents[contents_index];
+        Debug.Log("Displaying contents " + contents_index);
+        // Fade in the text
+        startTime = Time.time;
+        endTime = startTime + fadeInTime;
+        while (Time.time < endTime) {
+            alpha = (Time.time - startTime) / fadeInTime;
+            textObject.GetComponent<TextMeshProUGUI>().alpha = alpha;
+            yield return null;
+        }
+        // Display the text
+        yield return new WaitForSeconds(duration);
+        // Fade out the text
+        startTime = Time.time;
+        endTime = startTime + fadeOutTime;
+        while (Time.time < endTime) {
+            alpha = 1.0f - (Time.time - startTime) / fadeOutTime;
+            textObject.GetComponent<TextMeshProUGUI>().alpha = alpha;
+            yield return null;
+        }
+        // Delay before the next text
+        yield return new WaitForSeconds(delay);
+        // Start the next text
+        StartCoroutine(SchedulePrologue(contents_index + 1));
+    }
+}
