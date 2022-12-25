266c8d9bda724fc9e9e2d56d32af471213253494
PaulWang0513 <paul05130125@gmail.com>
2022-12-25 20:39:39 +0800

    stage0 almost done

--- Assets/Scripts/MyPlayTextEvents.cs
+++ Assets/Scripts/MyPlayTextEvents.cs
@@ -24,19 +24,21 @@ public class MyPlayTextEvents : MonoBehaviour
     void Start()
     {
         gameManager = GameObject.Find("GameManager");
-        // set playerCanMove and playerCanPossess to false
-        gameManager.GetComponent<GameManager>().SetPlayerCanMove(false);
-        gameManager.GetComponent<GameManager>().SetPlayerCanPossess(false);
 
         // adding events to EventCenter
         print("Adding PlayTextEvents");
         EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("PrintEvent", PrintEvent);
         EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("SetPlayerCanMove", SetPlayerCanMove);
-        EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("SetPlayerCanPossess", SetPlayerCanPossess);
+        EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("SetPangolinCanPossess", SetPangolinCanPossess);
+        EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("SetRabbitCanPossess", SetRabbitCanPossess);
         EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("WaitingForKeyAD", WaitingForKeyAD);
         EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("WaitingForKeyJL", WaitingForKeyJL);
         EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("WaitingForKeyW", WaitingForKeyW);
         EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("WaitingForKeyI", WaitingForKeyI);
+        EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("WaitingForPangolinPossessTable", WaitingForPangolinPossessTable);
+        EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("WaitingForRabbitPossessTable", WaitingForRabbitPossessTable);
+        EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("WaitingForKeyQ", WaitingForKeyQ);
+        EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("WaitingForKeyU", WaitingForKeyU);
         EventCenter.GetInstance().AddEventListener<List<EventValueClass>>("DestroyPlayText_Follow", DestroyPlayText_Follow);
 
         // start the graph
@@ -60,11 +62,18 @@ public class MyPlayTextEvents : MonoBehaviour
         gameManager.GetComponent<GameManager>().SetPlayerCanMove(Value[0].boolValue);
     }
 
-    // set playerCanPossess in event way
-    void SetPlayerCanPossess(List<EventValueClass> Value)
+    // set pangolinCanPossess in event way
+    void SetPangolinCanPossess(List<EventValueClass> Value)
     {
-        Debug.Log("SetPlayerCanPossess");
-        gameManager.GetComponent<GameManager>().SetPlayerCanPossess(Value[0].boolValue);
+        Debug.Log("SetPangolinCanPossess");
+        gameManager.GetComponent<GameManager>().SetPangolinCanPossess(Value[0].boolValue);
+    }
+
+    // set rabbitCanPossess in event way
+    void SetRabbitCanPossess(List<EventValueClass> Value)
+    {
+        Debug.Log("SetRabbitCanPossess");
+        gameManager.GetComponent<GameManager>().SetRabbitCanPossess(Value[0].boolValue);
     }
 
     // lock conversation and start waiting for player to press KeyA and KeyD
@@ -177,10 +186,121 @@ public class MyPlayTextEvents : MonoBehaviour
         EventCenter.GetInstance().EventTriggered("PlayText.ForceNext");
     }
 
+    // lock conversation and start waiting for Pangolin to possess a table
+    void WaitingForPangolinPossessTable(List<EventValueClass> Value)
+    {
+        Debug.Log("WaitingForPangolinPossessTable");
+        StartCoroutine(SchduleWaitingForPangolinPossessTable(Value));
+    }
+
+    // coroutine to wait for Pangolin to possess a table
+    IEnumerator SchduleWaitingForPangolinPossessTable(List<EventValueClass> Value)
+    {
+        Debug.Log("SchduleWaitingForPangolinPossessTable");
+        // lock conversation before waiting
+        EventCenter.GetInstance().EventTriggered("LockConversation");
+        // TODO: check if pangolin have possessed a table
+        // currently, just wait for keyQ
+        bool Q_pressed = false;
+        // wait for player to press KeyQ
+        while (!Q_pressed || Input.GetKey(KeyCode.Q))
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+                Q_pressed = true;
+            yield return null;
+        }
+        // unlock conversation after table is possessed
+        EventCenter.GetInstance().EventTriggered("UnLockConversation");
+        EventCenter.GetInstance().EventTriggered("PlayText.ForceNext");
+    }
+
+    // lock conversation and start waiting for Rabbit to possess a table
+    void WaitingForRabbitPossessTable(List<EventValueClass> Value)
+    {
+        Debug.Log("WaitingForRabbitPossessTable");
+        StartCoroutine(SchduleWaitingForRabbitPossessTable(Value));
+    }
+
+    // coroutine to wait for Rabbit to possess a table
+    IEnumerator SchduleWaitingForRabbitPossessTable(List<EventValueClass> Value)
+    {
+        Debug.Log("SchduleWaitingForRabbitPossessTable");
+        // lock conversation before waiting
+        EventCenter.GetInstance().EventTriggered("LockConversation");
+        // TODO: check if rabbit have possessed a table
+        // currently, just wait for keyU
+        bool U_pressed = false;
+        // wait for player to press KeyU
+        while (!U_pressed || Input.GetKey(KeyCode.U))
+        {
+            if (Input.GetKeyDown(KeyCode.U))
+                U_pressed = true;
+            yield return null;
+        }
+        // unlock conversation after table is possessed
+        EventCenter.GetInstance().EventTriggered("UnLockConversation");
+        EventCenter.GetInstance().EventTriggered("PlayText.ForceNext");
+    }
+
+    // lock conversation and start waiting for player to press KeyQ
+    void WaitingForKeyQ(List<EventValueClass> Value)
+    {
+        Debug.Log("WaitingForKeyQ");
+        StartCoroutine(SchduleWaitingForKeyQ(Value));
+    }
+
+    // coroutine to wait for player to press KeyQ
+    IEnumerator SchduleWaitingForKeyQ(List<EventValueClass> Value)
+    {
+        Debug.Log("SchduleWaitingForKeyQ");
+        // lock conversation before waiting
+        EventCenter.GetInstance().EventTriggered("LockConversation");
+        bool Q_pressed = false;
+        // wait for player to press KeyQ
+        while (!Q_pressed || Input.GetKey(KeyCode.Q))
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+                Q_pressed = true;
+            yield return null;
+        }
+        // unlock conversation after Q is pressed
+        EventCenter.GetInstance().EventTriggered("UnLockConversation");
+        EventCenter.GetInstance().EventTriggered("PlayText.ForceNext");
+    }
+
+    // lock conversation and start waiting for player to press KeyU
+    void WaitingForKeyU(List<EventValueClass> Value)
+    {
+        Debug.Log("WaitingForKeyU");
+        StartCoroutine(SchduleWaitingForKeyU(Value));
+    }
+
+    // coroutine to wait for player to press KeyU
+    IEnumerator SchduleWaitingForKeyU(List<EventValueClass> Value)
+    {
+        Debug.Log("SchduleWaitingForKeyU");
+        // lock conversation before waiting
+        EventCenter.GetInstance().EventTriggered("LockConversation");
+        bool U_pressed = false;
+        // wait for player to press KeyU
+        while (!U_pressed || Input.GetKey(KeyCode.U))
+        {
+            if (Input.GetKeyDown(KeyCode.U))
+                U_pressed = true;
+            yield return null;
+        }
+        // unlock conversation after U is pressed
+        EventCenter.GetInstance().EventTriggered("UnLockConversation");
+        EventCenter.GetInstance().EventTriggered("PlayText.ForceNext");
+    }
+
     // destroy PlayText_Follow
     void DestroyPlayText_Follow(List<EventValueClass> Value)
     {
         Debug.Log("DestroyPlayText_Follow");
+        gameManager.GetComponent<GameManager>().SetPlayerCanMove(true);
+        gameManager.GetComponent<GameManager>().SetPangolinCanPossess(true);
+        gameManager.GetComponent<GameManager>().SetRabbitCanPossess(true);
         GameObject to_destroy = GameObject.Find("PlayText_Follow");
         if (to_destroy != null)
             Destroy(GameObject.Find("PlayText_Follow"));
